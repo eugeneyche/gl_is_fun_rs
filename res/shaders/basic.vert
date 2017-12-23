a7283526afec4ccda65e70903fedf7794ad4e936
Eugene Che <eugeneyche@gmail.com>
2017-12-22 16:47:19 -0800

    Added basic 3D with cgmath

--- res/shaders/basic.vert
+++ res/shaders/basic.vert
@@ -1,11 +1,13 @@
 #version 330 core
 
-layout(location = 0) in vec2 position;
+layout(location = 0) in vec3 position;
 layout(location = 1) in vec2 uv;
+
+uniform mat4 wvp;
 
 smooth out vec2 frag_uv;
 
 void main() {
-    gl_Position = vec4(position, 0, 1);
+    gl_Position = wvp * vec4(position, 1);
     frag_uv = uv;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate cgmath;
 extern crate gl;
 extern crate glutin;
 extern crate image as piston_image;
@@ -9,6 +10,7 @@ mod shader_program;
 mod texture;
 mod vertex_array;
 
+use cgmath::prelude::*;
 use glutin::GlContext;
 use std::path::Path;
 
@@ -16,7 +18,7 @@ fn main() {
     let mut events_loop = glutin::EventsLoop::new();
     let window = glutin::WindowBuilder::new()
         .with_title("Open GL is fun!")
-        .with_dimensions(256, 328);
+        .with_dimensions(1024, 768);
     let context = glutin::ContextBuilder::new()
         .with_gl(
             glutin::GlRequest::Specific(
@@ -42,6 +44,7 @@ fn main() {
         ).unwrap(),
     ]).unwrap();
     let tex_loc = program.get_uniform_location("tex");
+    let wvp_loc = program.get_uniform_location("wvp");
     let img = image::Image::from_file(Path::new("res/images/squid.png")).unwrap();
     let mut squid_tex = texture::Texture::new().unwrap();
     squid_tex
@@ -58,31 +61,40 @@ fn main() {
         .bind()
         .upload(
             &[
-                [-1f32,  1., 0., 1.],
-                [ 1f32,  1., 1., 1.],
-                [-1f32, -1., 0., 0.],
-                [ 1f32, -1., 1., 0.],
+                [-1f32,  1.3, 0., 0., 1.],
+                [ 1f32,  1.3, 0., 1., 1.],
+                [-1f32, -1., 0., 0., 0.],
+                [ 1f32, -1., 0., 1., 0.],
             ],
             buffer::BufferUsageHint::DynamicDraw,
         );
     vertex_array
         .bind(vertex_buffer.bind())
         .set_vertex_attribs(
-            4 * 4,
+            5 * 4,
             &[
                 vertex_array::VertexAttrib {
-                    ty: vertex_array::VertexAttribTy::FloatVec2,
+                    ty: vertex_array::VertexAttribTy::FloatVec3,
                     offset: 0,
                     normalized: false,
                 },
                 vertex_array::VertexAttrib {
                     ty: vertex_array::VertexAttribTy::FloatVec2,
-                    offset: 2 * 4,
+                    offset: 3 * 4,
                     normalized: false,
                 },
             ],
         );
+    let projection = cgmath::perspective(cgmath::Rad(1f32),  1024.0 / 768.0, 0.1, 100.0);
+    let view = cgmath::Matrix4::look_at(
+        cgmath::Point3::new(0f32, 2.0, 10.0),
+        cgmath::Point3::new(0f32, 0.0, 0.0),
+        cgmath::Vector3::new(0f32, 1.0, 0.0),
+    );
+    let mut world = cgmath::Matrix4::<f32>::identity();
     while running {
+        world = world * cgmath::Matrix4::from_angle_y(cgmath::Rad(0.05f32));
+        let wvp = projection * view * world;
         events_loop.poll_events(|event| {
             match event {
                 glutin::Event::WindowEvent{ event, .. } => match event {
@@ -93,9 +105,10 @@ fn main() {
             }
         });
         unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
-        let mut bound_squid_tex = squid_tex.bind(0);
+        let bound_squid_tex = squid_tex.bind(0);
         let mut active_program = program.activate();
         active_program.uniform_integer(tex_loc, bound_squid_tex.unit_index as _);
+        active_program.uniform_float_mat4(wvp_loc, wvp.as_ref());
         vertex_array
             .bind(vertex_buffer.bind())
             .draw_arrays(vertex_array::DrawTy::TriangleStrip, 0, 4);
